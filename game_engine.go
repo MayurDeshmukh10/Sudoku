fa8e8f3824533e858209c1e44f06b4b1f7a7e66d
sagar23sj <sagarsonwane23@gmail.com>
2020-08-29 10:02:00 +0530

    Create Required Structure for Sudoku

--- game_engine.go
+++ game_engine.go
@@ -1,1 +1,15 @@
 package main
+
+type Sudoku struct {
+	sudokuGrid      [9][9]int      //Array for Storing Values in cells
+	replicatedGrid  [9][9]int      //Replica of Original Grid Before removing values
+	answerGrid      [9][9]int      //Storing ANswers from Users
+	difficultyLevel map[string]int //Map Storing K values based on level
+	gridSize        int            //Size of Puzzle
+	blockSize       int            //Size of a block
+	gameLevel       string         //Game Level Chosen by Player
+}
+
+func main() {
+
+}
