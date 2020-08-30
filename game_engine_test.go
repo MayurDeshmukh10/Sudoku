63e0ba41b1bd009a2ed5a066577fe6439a2b018a
sagar23sj <sagarsonwane23@gmail.com>
2020-08-30 14:01:12 +0530

    Removed removeKCells Test

--- game_engine_test.go
+++ game_engine_test.go
@@ -42,26 +42,3 @@ func TestInitializeGame(t *testing.T) {
 		t.Error("Game Level didnot match")
 	}
 }
-
-func TestRemoveKCells(t *testing.T) {
-	s := Sudoku{}
-	GridSize := 9
-	BlockSize := 3
-	Level := "0"
-	s.initializeGame(GridSize, BlockSize, Level)
-	s.createPuzzle(Level)
-
-	count := 0
-
-	for i := 0; i < s.gridSize; i++ {
-		for j := 0; j < s.gridSize; j++ {
-			if s.sudokuGrid[i][j] == 0 {
-				count = count + 1
-			}
-		}
-	}
-
-	if !assert.Equal(t, count, s.difficultyLevel[s.gameLevel]) {
-		t.Error("Number of Removed Cells not matched to the specification")
-	}
-}
